e2364cf1bcf6f92def84722b81bdacfdcdb476e7
11JuanK11 <juan_alzate82221@elpoli.edu.co>
2023-09-17 18:37:54 -0500

    Punto 2 - Incompleto

--- src/regular_expresion/Regular_Expresion.java
+++ src/regular_expresion/Regular_Expresion.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
@@ -14,6 +14,7 @@ public class Regular_Expresion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Punto2.Vista2();
     }
     
 }
