8a843db9f4d8bb169ebc38aae62eb8059135aef4
Caro-26S <sara_sanchez82221@elpoli.edu.co>
2023-09-17 16:52:03 -0500

    Punto3

--- src/Puntos_regex/Punto5.java
+++ src/Puntos_regex/Punto5.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Puntos_regex;
 
-/**
- *
- * @author 57301
- */
 public class Punto5 {
     
 }
